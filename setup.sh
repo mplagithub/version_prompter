8e652446a3627c1af0a385659ccbacc55eb77fa9
mplata <mariusz.plata@volvocars.com>
2025-08-25 13:11:09 +0200

    Initial version - checking if setup works

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOOK_SOURCE="./version_prompter/pre-commit"
+
+for repo in ../*/.git; do
+    hooks_dir="$repo/hooks"
+    if [ -d "$hooks_dir" ]; then
+        cp "$HOOK_SOURCE" "$hooks_dir/pre-commit"
+        chmod +x "$hooks_dir/pre-commit"
+        echo "Installed pre-commit hook in $hooks_dir"
+    fi
+done
