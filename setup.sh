fc3f47c1d44b8d11c8874bf5549e15568b9ae436
mplata <mariusz.plata@volvocars.com>
2025-08-25 13:25:10 +0200

    tiny fix to setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-HOOK_SOURCE="pre-commit"
+HOOK_SOURCE="./pre-commit"
 
 for repo in ../*/.git; do
+    # Skip version_prompter directory
+    if [[ "$repo" == *"/version_prompter/.git" ]]; then
+        continue
+    fi
     hooks_dir="$repo/hooks"
     if [ -d "$hooks_dir" ]; then
         cp "$HOOK_SOURCE" "$hooks_dir/pre-commit"
